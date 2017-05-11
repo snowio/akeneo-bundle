abe318bd3215c91ddd7121b07b35ff28ab91843b
Nei Santos <ns@amp.co>
2017-05-11 09:47:06 +0100

    Import class to WriterTrait

--- Writer/File/Csv/WriterOverriderTrait.php
+++ Writer/File/Csv/WriterOverriderTrait.php
@@ -2,6 +2,8 @@
 
 namespace Snowio\Bundle\CsvConnectorBundle\Writer\File\Csv;
 
+use Akeneo\Component\Batch\Job\JobInterface;
+
 trait WriterOverriderTrait
 {
     /**
@@ -27,9 +29,12 @@ trait WriterOverriderTrait
     }
 
     /**
-     * Override Export medias to avoid deleting the files path since it is shared.
+     * Export medias from the working directory to the output expected directory.
      *
-     * More info see https://github.com/AmpersandHQ/bes-akeneo/pull/46
+     * Basically, we first remove the content of /path/where/my/user/expects/the/export/files/.
+     * (This path can exist of an export was launched previously)
+     *
+     * Then we copy /path/of/the/working/directory/files/ to /path/where/my/user/expects/the/export/files/.
      */
     protected function exportMedias()
     {
@@ -40,7 +45,7 @@ trait WriterOverriderTrait
         $outputFilesDirectory = $outputDirectory . DIRECTORY_SEPARATOR . 'files';
         $workingFilesDirectory = $workingDirectory . 'files';
 
-        /* Avoid removing files folder
+        /*
         if ($this->localFs->exists($outputFilesDirectory)) {
             $this->localFs->remove($outputFilesDirectory);
         }*/
