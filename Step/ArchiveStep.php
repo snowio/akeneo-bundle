9fa5058e9848bbc6086a49d13e561c02a6d64cdd
dank00 <dk@amp.co>
2017-10-02 16:41:42 +0100

    Include media export log for the job in zip archive
    - run the archive step after the media export

--- Step/ArchiveStep.php
+++ Step/ArchiveStep.php
@@ -6,6 +6,7 @@ use Akeneo\Component\Batch\Step\AbstractStep;
 use Akeneo\Component\Batch\Model\StepExecution;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Akeneo\Component\Batch\Job\JobRepositoryInterface;
+use Snowio\Bundle\CsvConnectorBundle\MediaExport;
 use \ZipArchive;
 
 class ArchiveStep extends AbstractStep
@@ -15,22 +16,28 @@ class ArchiveStep extends AbstractStep
     /** @var ZipArchive */
     protected $zip;
 
+    /** @var \Snowio\Bundle\CsvConnectorBundle\MediaExport\Logger */
+    private $mediaExportLogger;
+
     /**
      * @param string                   $name
      * @param EventDispatcherInterface $eventDispatcher
      * @param JobRepositoryInterface   $jobRepository
      * @param ZipArchive               $zip
+     * @param string                   $logDir
      */
     public function __construct(
         $name,
         EventDispatcherInterface $eventDispatcher,
         JobRepositoryInterface $jobRepository,
-        ZipArchive $zip
+        ZipArchive $zip,
+        MediaExport\Logger $mediaExportLogger
     ) {
         $this->name = $name;
         $this->jobRepository = $jobRepository;
         $this->eventDispatcher = $eventDispatcher;
         $this->zip = $zip;
+        $this->mediaExportLogger = $mediaExportLogger;
     }
 
     /**
@@ -50,6 +57,11 @@ class ArchiveStep extends AbstractStep
         if ($opened !== true) {
             $stepExecution->addFailureException(new \RuntimeException('Failed to open zip, reason code:' . $opened));
         } else {
+            $this->zip->addFile(
+                $this->mediaExportLogger->getLogFileNameForJob($stepExecution->getJobExecution()->getId()),
+                '/media_export.log'
+            );
+
             $success = $this->zip->addPattern(
                 '/(?:\w+\.csv|metadata.json)/',
                 $jobParameters->get('exportDir'),
